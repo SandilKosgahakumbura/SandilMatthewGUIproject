2afb9304247bc8ae7e6d8e0642a8d1fb20a64002
MatthewGormley05 <matthewgormley005@gmail.com>
2024-05-11 17:19:24 +0100

    The beginnings of the Searchbar testing

---
+++ JS/SearchBar.js
@@ -0,0 +1,2 @@
+//The code will work by using one value to compare two arrays.
+//eg the value 1 will point to the name "bananas" on the first array and point to the url "bananas.com" on the second array
