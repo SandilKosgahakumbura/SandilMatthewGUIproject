8334db09cbc92712e090b97283a1dbf8eb8a3761
MatthewGormley05 <matthewgormley005@gmail.com>
2024-05-12 22:34:18 +0100

    Updated Search Bar, Basic Entry Add, + Testing

---
+++ JS/AddEntry.js
@@ -0,0 +1,6 @@
+function addUserEntry()
+{
+    entryName.push(createName.value);
+    entryURL.push(createURL.value);
+    alert("Added new entry to archives.");
+}
--- JS/SearchBar.js
+++ JS/SearchBar.js
@@ -1,26 +1,30 @@
 //The code will work by using one value to compare two arrays.
 //eg the value 1 will point to the name "bananas" on the first array and point to the url "bananas.com" on the second array
-//<script src="../JS/SearchBar.js"></script>
 
-const entryName = ["test 1", "test 2", "test 3"];
-const entryURL = ["other 1", "other 2", "other 3"];
+const entryName = ["Google", "Wikipedia", "Github"];
+const entryURL = ["https://google.com", "https://www.wikipedia.org/", "https://github.com/"];
 
 //aC stands for Array Count
 let aC = 0;
-let input = "";
+let input = ""; //It's in string form so it can be compared to the arrays
 
 //Function will prompt user to enter a search value and then compare it to the array
-function userSearch(){
-    for(aC = 0; aC <= entryName.length; aC++)
-        {
-            input = search.value;
+function userSearch()
+{
+    for(aC = 0; aC < entryName.length; aC++)
+    {
+        input = search.value;
+        input = input.toString();
+
+        let currentName = entryName[aC];
+        let currentURL = entryURL[aC];
             
-            if(input = entryName[1]) {
-                alert("match has been found!");
-                document.getElementById("nameHolder").innerHTML = document.write(entryName[1]);
-                document.getElementById("URLHolder").innerHTML = document.write(entryURL[1]);
-            }
+        if(input.toUpperCase() == currentName.toUpperCase())
+        {
+            alert("match has been found!");
+            document.getElementById("nameHolder").innerHTML = '<a href="'+currentURL+'">'+currentName+'</a>';
+            //document.getElementById("URLHolder").innerHTML = (currentURL);
+            aC = entryName.length + 1;
         }
+    }
 }
-
-
