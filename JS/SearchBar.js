e0878c640d186b02950c038ab5f8a6d8179d9495
MatthewGormley05 <matthewgormley005@gmail.com>
2024-05-13 01:43:47 +0100

    Deleted "Add Entry", finished Search Bar

--- JS/SearchBar.js
+++ JS/SearchBar.js
@@ -1,8 +1,10 @@
 //The code will work by using one value to compare two arrays.
 //eg the value 1 will point to the name "bananas" on the first array and point to the url "bananas.com" on the second array
 
-const entryName = ["Google", "Wikipedia", "Github", "Test 1", "Test 2", "Test 3"];
-const entryURL = ["https://google.com", "https://www.wikipedia.org/", "https://github.com/", "../Archive Entries (HTML)/Test Entry 1.html", "../Archive Entries (HTML)/Test Entry 2.html", "../Archive Entries (HTML)/Test Entry 3.html"];
+const entryName = ["Test 1", "Test 2", "Test 3"];
+const entryURL = ["../Archive Entries (HTML)/Test Entry 1.html", "../Archive Entries (HTML)/Test Entry 2.html", "../Archive Entries (HTML)/Test Entry 3.html"];
+const cutArrayName = [];
+const cutArrayURL = [];
 
 //aC stands for Array Count
 let aC = 0;
@@ -13,24 +15,52 @@ function userSearch()
 {
     for(aC = 0; aC < entryName.length; aC++)
     {
+        //input value imported from html, made case insensitive with rest of code
         input = search.value;
-        input = input.toString();
+        input = input.toString().toLowerCase();
 
-        let currentName = entryName[aC];
+        let currentName = entryName[aC].toLowerCase();
         let currentURL = entryURL[aC];
 
-        //Compare search input to current entry name. If the result isn't -1, add it to an array. Then show the results of this array
+        //Compare search input to current entry name. If the result isn't -1, add it to the cut array. Then show the results of this array
         let cutSearch = currentName.search(input);
-        alert(cutSearch);
+        let cutLoop = 0;
+        let cutName = "test";
+        let cutURL = "test";
+
+        if(cutSearch != -1)
+        {
+            cutArrayName.push(currentName);
+            cutArrayURL.push(currentURL);
+        }
+
+        let cutResult = "";
+
+        if(aC <= entryName.length)
+            {
+                for(cutLoop = 0; cutLoop < cutArrayName.length; cutLoop++)
+                {
+                    cutName = cutArrayName[cutLoop];
+                    cutURL = cutArrayURL[cutLoop];
+                    cutResult += '<br><a href="' + cutURL + '">' + cutName + '</a>';
+                }
+
+                cutArrayName.forEach(function(item) {
+                    console.log(item);
+                });
+            }
         
-        
+        document.getElementById("nameHolder").innerHTML = cutResult;
+
+        //This is the old testing code
+        /*
         if(input.toUpperCase() == currentName.toUpperCase())
         {
             alert("match has been found!");
             document.getElementById("nameHolder").innerHTML = '<a href="'+currentURL+'">'+currentName+'</a>';
             aC = entryName.length + 1;
         }
-        
+        */
 
         
     }
